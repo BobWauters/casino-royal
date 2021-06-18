f2f261833b3b19511a6b16accf46978910eb739d
BobW <biz.wautersbob@gmail.com>
2021-06-18 11:01:21 +0200

    JS click functions

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
-//TODO:Figure out a way to know when the "play" button was clicked (events and handling off)
+
+/* 1. Create the HTML you need
+    - Three buttons for the player to choose from
+    - Player button to trigger the computer's turn
+    - A message we can use to show the result. Put some temporary content ([a random message]()http://www.cupcakeipsum.com) there to test the styling
+2. Add some *basic* styling for those
+3. Figure out a way to know when the "play" button was clicked (tip: you'll need `events` in JavaScript for that, and you'll need to `handle` those)
+4. Next up, we want to determine the computer's choice
+    - How many choices does the computer have? What can be a suitable datatype to store multiple things?
+    - How can you select a random element from these choices? The math options in JavaScript might help you here
+5. Determine the payers result: draw / win / lose. It's okay if this involves a lot of conditional statements (if)
+6. Depending on the result, change the `inner html` (hint hint) of the message to congratulate or comfort the player
+*/
+
+(function (){
+
+    console.log('test excercise')
+
+    const playerChoices =[ 'rock', 'paper', 'scissors'];
+    let playerChoice;
+    document.getElementById('rock').addEventListener('click', function (){
+       console.log('rock')
 
 
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ script2.js
@@ -0,0 +1,21 @@
+(function () {
+    console.log('test excercise')
+
+
+    const playerChoices =[ 'rock', 'paper', 'scissors'];
+    let playerChoice;
+
+    document.getElementById('rock').addEventListener('click', function (){
+        console.log('rock')
+    });
+    document.getElementById('paper').addEventListener('click', function (){
+        console.log('paper')
+    });
+    document.getElementById('scissors').addEventListener('click', function (){
+        console.log('scissors')
+    });
+
+
+
+
+    })();
